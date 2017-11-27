e9fb8560a506efb53997f31712c3c0709b5ec553
DESKTOP-QTMM0KK\zerg <trescins@gmail.com>
2017-11-27 21:54:36 +0100

    Added contact-form

---
+++ mail.php
@@ -0,0 +1,10 @@
+<?php
+$to      = 'trescins@gmail.com';
+$subject = 'Mail ze strony internetowej';
+$email   = $_POST['email_adress'];
+$message = $_POST['message'];
+$headers = 'From: ' . $email;
+$headers .= "Content-Type: text/html; charset=utf.8\r\n";
+
+mail($to, $subject, $message, $headers);
+?>
