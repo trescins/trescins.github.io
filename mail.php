b7186a84103db28260421b924bb6290607a81461
DESKTOP-QTMM0KK\zerg <trescins@gmail.com>
2017-11-28 09:05:58 +0100

    change form contact, added server ?

--- mail.php
+++ mail.php
@@ -3,8 +3,8 @@ $to      = 'trescins@gmail.com';
 $subject = 'Mail ze strony internetowej';
 $email   = $_POST['email_adress'];
 $message = $_POST['message'];
-$headers = 'From: ' . $email;
-$headers .= "Content-Type: text/html; charset=utf.8\r\n";
+$headers = 'From: ' $email;
+$headers = "Content-Type: text/html; charset=utf.8\r\n";
 
 mail($to, $subject, $message, $headers);
 ?>
